0112e4d5f6ee73965520d2dc1047751c21b9108d
Alisha Angdembe <hhy797@ddy110.apocrita>
2024-06-14 11:05:08 +0100

    Remove Burst directory and its contents

--- Burst/.ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,3 +0,0 @@
-from trains import train_detection, calculate_train_info, calculate_scores_train
-from scores import calculate_scores
-
--- Burst/__init__.py
+++
@@ -1,4 +0,0 @@
-from trains import train_detection, calculate_train_info, calculate_scores_train
-from scores import calculate_scores
-
-
