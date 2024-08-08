052e9bf4de2b96c3b406eb42db1d016e3a9b41e9
Alisha Angdembe <hhy797@ddy108.apocrita>
2024-08-08 18:22:06 +0100

    version change

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ DESCRIPTION = 'bursty_dynamics is a Python package designed to facilitate the an
     
 setup(
     name= 'bursty_dynamics',
-    version = '0.1.0', 
+    version = '0.1', 
     description = DESCRIPTION,
     packages = find_packages(),
     long_description = long_description,
